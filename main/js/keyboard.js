d54062c7a51e14fcbd0e856381afbc32d1a4ea2c
iSkore <iSkore@users.noreply.github.com>
2017-06-13 19:21:48 +0000

    Add files via upload

---
+++ main/js/keyboard.js
@@ -0,0 +1,52 @@
+'use strict';
+
+class KeyboardAction
+{
+    constructor( code, onPress, onRelease )
+    {
+        this.code = code;
+        this.isDown = false;
+        this.isUp = true;
+        this.press = onPress || ( () => {} );
+        this.release = onRelease || ( () => {} );
+
+        this.init();
+    }
+
+    init()
+    {
+        window.addEventListener(
+            'keydown', this.onPressed.bind( this ), false
+        );
+
+        window.addEventListener(
+            'keyup', this.onReleased.bind( this ), false
+        );
+    }
+
+    onPressed( e )
+    {
+        e.preventDefault();
+
+        if( e.keyCode === this.code ) {
+            if( this.isUp && this.press )
+                this.press();
+            this.isDown = true;
+            this.isUp = false;
+        }
+    }
+
+    onReleased( e )
+    {
+        e.preventDefault();
+
+        if( e.keyCode === this.code ) {
+            if( this.isDown && this.release )
+                this.release();
+            this.isDown = false;
+            this.isUp = true;
+        }
+    }
+}
+
+module.exports = KeyboardAction;
